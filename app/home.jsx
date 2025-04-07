a08d5e53253d26968903ce80db147588c5536262
Yuvraj Khandelwal <yuvraj.khandelwal.509@gmail.com>
2025-04-07 15:00:17 +0530

    Initial commit

---
+++ app/home.jsx
@@ -0,0 +1,51 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import ImageSlider from "../components/ImageSlider";
+
+export default function Home() {
+  return (
+    <SafeAreaView className="flex-1 bg-white flex gap-y-5" edges={["top"]}>
+      <StatusBar style="dark" />
+
+      {/* punchline and avatar */}
+      <View className="flex-row justify-between items-center mx-5">
+        <View className="gap-y-2">
+          <Text
+            style={{ fontSize: hp(6) }}
+            className="font-bold tracking-wider text-neutral-700"
+          >
+            Ready To
+          </Text>
+          <Text
+            style={{ fontSize: hp(6) }}
+            className="font-bold tracking-wider text-rose-500"
+          >
+            Workout
+          </Text>
+        </View>
+        <View className="flex justify-center items-center gap-y-2">
+          <Image
+            source={require("../assets/images/avatar.png")}
+            style={{ height: hp(8), width: wp(8), borderRadius: hp(4) }}
+            resizeMode="cover"
+          />
+          <View className="bg-neutral-200 rounded-full flex justify-center items-center border-[3px] border-neutral-300">
+            <Ionicons name="notifications" size={hp(5)} color="grey" />
+          </View>
+        </View>
+      </View>
+
+      {/* image slider */}
+      <View>
+        <ImageSlider />
+      </View>
+    </SafeAreaView>
+  );
+}
---
+++ components/ImageSlider.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Carousel from 'react-native-snap-carousel'
+
+const ImageSlider = () => {
+  return (
+    <Carousel />
+  )
+}
+
+export default ImageSlider
